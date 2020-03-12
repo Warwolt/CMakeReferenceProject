a5c7cde47f6bb9251a38547fc290e12a23eff92a
Rasmus Källqvist <rasmus.kallqvist@mojang.com>
2020-03-12 18:15:32 +0100

    Add test dir and fake unit test file

---
+++ software/module_hello/test/hello_test.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+
+// this function called from testmain.cpp
+void sayHelloTests()
+{
+    sayHello(); // calling into function here just to make sure we're linking
+}
--- software/testmain.cpp
+++ software/testmain.cpp
@@ -2,11 +2,13 @@
 #include "hello.h"
 #include "extlib.h" // mock thirdparty dependency
 
+void sayHelloTests(); // faking automatic test suite detection
+
 int main()
 {
     // mocking a test runner
     std::cout << identity("Runnings tests...") << std::endl; // use mock 3rd party library 'identity' function
-    sayHello();
+    sayHelloTests();
     std::cout << "All tests OK" << std::endl;
     return 0;
 }
